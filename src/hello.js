837f051b89daef5ec5ae8a4747803e08b8a14506
Nahidul Islam <nahidkishore99@gmail.com>
2023-08-06 19:28:00 +0600

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello Serverless!",
+        message: "Hello Serverless!!!",
       },
       null,
       2
